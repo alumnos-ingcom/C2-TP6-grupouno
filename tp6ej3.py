3408240cd38a0c7d05766c8f2cde128e6d0e39e9
ANDRES BOCCHIGLIERE <elmaildeandresbochi@gmail.com>
2021-06-25 17:26:03 -0300

    Agregada excepcion de error FileNotFoundError

--- tp6ej3.py
+++ tp6ej3.py
@@ -8,17 +8,31 @@
 # Crear un programa que solicite al usuario un archivo de entrada y
 # uno de salida para copiar el contenido de un archivo en el otro.
 
+
+class ArchivoNoEncontrado(Exception):
+    pass
+
+def abrir_archivo(archivo, modo):
+    try:
+        archivo_abierto = open(archivo, modo, encoding="UTF-8")
+        
+    except FileNotFoundError as err:
+        raise ArchivoNoEncontrado("No se encontró el archivo") from err
+    
+    return archivo_abierto
+    
 def copiadora(archivo_entrada, archivo_salida):
     """
     Esta función sobreescribe el contenido de un archivo .txt de entrada (1er argumento)
         en otro .txt de salida (2do argumento)
     
     """
-    with open(archivo_entrada, "r", encoding="UTF-8") as entrada:
-        with open(archivo_salida, "w", encoding="UTF-8") as salida:
-            for linea in entrada:
-                salida.write(linea)
-                
+    entrada = abrir_archivo(archivo_entrada, "r")
+    salida = abrir_archivo(archivo_salida, "w")
+    
+    for linea in entrada:
+        salida.write(linea)
+    
 def principal():
     
     print("Programa Copiadora:")
@@ -31,9 +45,14 @@ def principal():
     print("\nAhora indique el nombre del archivo de destino: (Formato de ingreso: Nombre.txt)")
     archivo2 = str(input())
     
-    print("\nRealizando copia")
-    copiadora(archivo1, archivo2)
-    print("\n¡Copia finalizada!")
+    try:
+        print("\nRealizando copia")
+        copiadora(archivo1, archivo2)
+        print("\n¡Copia finalizada!")
+    except ArchivoNoEncontrado:
+        print("\n¡No se encontró el archivo!")
+    
+    
 
 if __name__ == "__main__":
     principal()
